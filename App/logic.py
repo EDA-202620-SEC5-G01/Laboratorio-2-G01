37a9a2cda4166065c5fd323700c5963b99872bdc
mateo-uniandes <fm.castrof1@uniandes.edu.co>
2026-08-18 21:55:38 -0500

    .

--- App/logic.py
+++ App/logic.py
@@ -137,7 +137,7 @@ def load_books_tags(catalog, filename):
     input_file = csv.DictReader(open(booktagfile, encoding="utf-8"))
     catalog = create_book_tag_list(catalog)
     for booktag in input_file:
-    add_book_tag(catalog, booktag)
+        add_book_tag(catalog, booktag)
     return book_tag_size(catalog)
 
 
@@ -156,7 +156,7 @@ def last_book(catalog):
     Devuelve el ultimo libro del catalogo
     """
     # TODO: Mods Est-3 en el Lab 2
-    return set.get_last element(catalog["books"])
+    return set.get_last_element(catalog["books"])
 
 
 # Funciones para la manipulacion de los datos
